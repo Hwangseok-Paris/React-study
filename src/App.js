22d1817c1fb4b10b7dc8d1d1cb00ac32a16baffd
Hwangseok-Paris <hwangseok.paris@gmail.com>
2021-07-23 18:41:36 +0900

    210723 1839 : React 강의 15.3 까지 완료항목   - Component   - props   - state   - React developer Tool

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
+import TOC from './components/TOC';
+import Content from './components/Content'
+import Subject from './components/Subject'
 import { Component } from 'react';
 
+
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      test1:{title:"WEB", sub:"World Wide Web!!! YEAH!!"},
+      test2:{sub:"test용"},
+      contents:[
+        {id:1, title:"HTML", desc:"HTML is for information"},
+        {id:2, title:"CSS", desc:"CSS is for design"},
+        {id:3, title:"JavaScript", desc:"JavaStript is interactive"}
+      ]
+    }
+  }
+
   render() {
     return(
       <div className="App"> 
-        
-          React Start!! <br></br>
-          우와 깃 허브도 연동했다!
-        
+          <Subject title={this.state.test1.title} sub={this.state.test2.sub}></Subject>
+          <Subject title="React" sub="For UI"></Subject>
+          <TOC data={this.state.contents}></TOC>
+          <Content title="HTML" desc="HTML is HyperText Markup Languate."></Content>
       </div>
     );
   }
